63dccb092a11dd654cfedbc58478759c9a746752
ambud <asharma525xi@gmail.com>
2016-07-07 18:51:03 -0700

    Adding environment variable checking

--- configure.sh
+++ configure.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ -z $ZOOKEEPER_HOST ]; then
+  echo "Missing ZOOKEEPER_HOST environment variable"
+  exit
+fi
+
+if [ -z $ZOOKEEPER_PORT ]; then
+  echo "Missing ZOOKEEPER_PORT environment variable"
+  exit
+fi
+
+if [ -z $NIMBUS ]; then
+  echo "Missing NIMBUS environment variable"
+  exit
+fi
+
 envsubst < /opt/storm/conf/storm.yaml.template > /opt/storm/conf/storm.yaml
 envsubst < /opt/storm/conf/storm_env.ini.template > /opt/storm/conf/storm_env.ini
 
