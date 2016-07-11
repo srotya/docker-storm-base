9a083d1f03f5283c2b696e384c61d78d4daee17b
ambud <asharma525xi@gmail.com>
2016-07-10 22:40:07 -0700

    Fixing configuration file

--- configure.sh
+++ configure.sh
@@ -19,5 +19,5 @@ fi
 envsubst < /opt/storm/conf/storm.yaml.template > /opt/storm/conf/storm.yaml
 envsubst < /opt/storm/conf/storm_env.ini.template > /opt/storm/conf/storm_env.ini
 
-env >> /opt/storm/conf/storm.yaml
-env >> /opt/storm/conf/storm_env.ini
+#env | sed -e 's/=/:/g' >> /opt/storm/conf/storm.yaml
+#env | sed -e 's/=/:/g' >> /opt/storm/conf/storm_env.ini
